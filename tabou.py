0a113961301b6e29982400d386599e285d8d856a
BebertDeChast <hdechastellux@gmail.com>
2024-01-09 16:35:29 +0100

    maj

--- tabou.py
+++ tabou.py
@@ -18,7 +18,7 @@ def getValue(solution, nb_machines):
     return ordo['disponibilité'][nb_machines-1]
 
 
-def getNeighbours(solution):
+def getNeighbours_mono_swap(solution):
     neighboursList = []
     indexes = [i for i in range(len(solution))]
     idx = indexes.pop(randint(0, len(indexes)-1))
@@ -27,14 +27,18 @@ def getNeighbours(solution):
         neighboursList[-1][idx], neighboursList[-1][i] = neighboursList[-1][i], neighboursList[-1][idx]
     return neighboursList
 
-
-def tabou(flow_shop, maxTabou=20):
+def tabou(flow_shop, maxTabou):
 
     nb_machines = flow_shop['nombre machines']
     solution_init = flowshop.liste_NEH(flow_shop)
     # Initialisation
     best = solution_init.copy()
     bestCandidate = solution_init.copy()
+
+    ordo = flowshop.creer_ordo_vide(nb_machines)
+    flowshop.ordonnancer_liste_jobs(ordo,best)
+    flowshop.afficher_ordo(ordo)
+
     tabouList = queue.Queue(maxsize=maxTabou)
     add_tabou(tabouList, solution_init)
     i = 0
@@ -42,13 +46,14 @@ def tabou(flow_shop, maxTabou=20):
     print("imax = ", imax)
     while i < imax:
         i += 1
-        listNeighbours = getNeighbours(bestCandidate)
+        listNeighbours = getNeighbours_mono_swap(bestCandidate)
         for candidate in listNeighbours:
             if candidate not in tabouList.queue:
                 candidateValue = getValue(candidate, nb_machines)
                 if candidateValue < getValue(bestCandidate, nb_machines):
+                    print("candidateValue = ", candidateValue)
                     bestCandidate = candidate.copy()
-        if getValue(bestCandidate, nb_machines) > getValue(best, nb_machines):
+        if getValue(bestCandidate, nb_machines) < getValue(best, nb_machines):
             best = bestCandidate.copy()
         add_tabou(tabouList, bestCandidate)
 
@@ -58,6 +63,6 @@ def tabou(flow_shop, maxTabou=20):
     flowshop.afficher_ordo(ordo)
 
 
-def tabouFromFile(fs_path, maxTabou=20):
+def tabouFromFile(fs_path, maxTabou=50):
     fs = flowshop.lire_flowshop(fs_path)
     return tabou(fs, maxTabou)
